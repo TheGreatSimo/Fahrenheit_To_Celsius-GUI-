783fd00f03581ad1d84538d63996cfb0477a8ff3
TheGreatSimo <aissaouicoder@gmail.com>
2024-09-13 07:28:44 +0100

    Functions for convertsion from fahrenheit to celsius are and rev are done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+   let mut ftc = fahrenheit_to_celsius(30);
+   println!("fahrenheit to celsius is {} ",ftc)
+
+//   let ctf = celsius_to_fahrenheit(-1);
+//   println!("celsius to fahrenheit is {} ",ctf)
+}
+
+fn fahrenheit_to_celsius(x:i32) -> f64 {
+    (((x as f64 -32 as f64) * 5 as f64) / 9 as f64)
+}
+
+fn celsius_to_fahrenheit(x:i32) -> f64 {
+    ((x as f64 * 9 as f64) / 5 as f64) + 32 as f64
 }
